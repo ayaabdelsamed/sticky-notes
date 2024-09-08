fb50fd177c2da160da06d5760ab0c9f773af2b48
Aya Abdelsamed <aya.abdelsamed11pp@gmail.com>
2024-09-08 13:31:49 +0300

    Json Web Token

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,18 +1,20 @@
 import { userModel } from "../../../databases/models/user.model.js"
 import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
+
 const signup= async(req,res)=>{
 
     await userModel.insertMany(req.body)
     res.json({message:'success'})
 }
 
-
 const signin= async(req,res)=>{
     let user = await userModel.findOne({email:req.body.email})
 
-
-    if(user&&bcrypt.compareSync(req.body.password,user.password)) 
-        return res.json({message:"login .. token"})
+    if(user&&bcrypt.compareSync(req.body.password,user.password)){
+        let token = jwt.sign({userID:user._id,role:user.role},'myNameIsAya')
+        return res.json({message:"success",token})
+    }
 
     res.json({message:"incorrect email or password"})
 }
